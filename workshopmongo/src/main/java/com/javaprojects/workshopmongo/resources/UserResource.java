c02a8d4af735d6bf51310d5b0c4773b3f851f380
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-05-14 16:48:05 -0300

    Getting user by id

--- workshopmongo/src/main/java/com/javaprojects/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/javaprojects/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,14 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok().body(listDto);
+		return ResponseEntity.ok(listDto);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		UserDTO userDto = new UserDTO(user);
+		
+		return ResponseEntity.ok(userDto);
+	}
 }
--- workshopmongo/src/main/java/com/javaprojects/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/javaprojects/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.javaprojects.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaprojects.workshopmongo.domain.User;
 import com.javaprojects.workshopmongo.repository.UserRepository;
+import com.javaprojects.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,15 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+	    Optional<User> user = repo.findById(id);
+
+	    if (user.isEmpty()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+
+	    return user.get();
 	}
 }
